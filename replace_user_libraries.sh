0314fcf113a5718ca70d97e59be6c33dcae6f537
Manikandan Ramakrishnan <mankand007@gmail.com>
2018-09-03 00:42:34 -0400

    initial commit

---
+++ replace_user_libraries.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+rm -rf ~/Documents
+ln -s /data/Documents ~/Documents
+rm -rf ~/Downloads
+ln -s /data/Downloads ~/Downloads
+rm -rf ~/Pictures
+ln -s /data/Pictures ~/Pictures
+rm -rf ~/Videos
+ln -s /data/Videos ~/Videos
+rm -rf ~/Music
+ln -s /data/Music ~/Music
+ 
