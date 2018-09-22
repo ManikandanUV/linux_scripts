900f3e772da82fe85122c1e51c186e4734c84c0c
Manikandan Ramakrishnan <manikandan@arch-GT72-6QD.localdomain>
2018-09-22 03:34:29 -0400

    better script for pacman time since

--- headphones_autorouting.sh
+++ headphones_autorouting.sh
@@ -7,10 +7,10 @@ while read -u "${COPROC[0]}" -a event; do
 	then
 		if [ "${event[2]}" = 'plug' ]
 		then
-			pacmd set-card-profile 1 output:iec958-stereo+input:analog-stereo
+			pacmd set-card-profile alsa_card.pci-0000_00_1f.3 output:iec958-stereo+input:analog-stereo
 		elif [ "${event[2]}" = 'unplug' ]
 		then
-			pacmd set-card-profile 1 output:analog-stereo+input:analog-stereo
+			pacmd set-card-profile alsa_card.pci-0000_00_1f.3 output:analog-stereo+input:analog-stereo
 		fi
 	fi
 done
---
+++ pacman_last_update.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+TIMESTAMP=$(cat /home/manikandan_arch/.pacman_last_update_timestamp)
+PACMAN_LAST_UPDATE_CHECKED=$(cat /var/log/pacman.log | grep -n 'starting full system upgrade' | tail -n1 | cut -d':' -f1)
+
+PACMAN_NEXT_OPERATION=$(tail -n+$(($PACMAN_LAST_UPDATE_CHECKED + 1)) /var/log/pacman.log | head -n1)
+NEXT_OPERATION=$(echo $PACMAN_NEXT_OPERATION | cut -d']' -f3)
+NEW_TIMESTAMP=$(echo $PACMAN_NEXT_OPERATION | cut -d']' -f1 | cut -d'[' -f2)
+if [ "$NEXT_OPERATION" = ' transaction started' ]
+then
+	TIMESTAMP=$NEW_TIMESTAMP
+	echo $TIMESTAMP > /home/manikandan_arch/.pacman_last_update_timestamp
+fi
+
+TIME_SINCE=$(($(date +%s) - $(date --date="$TIMESTAMP" +%s)))
+FORMATTED_TIME_SINCE=$(date -u -d @"$TIME_SINCE" +'%Hh %Mm')
+echo updated $FORMATTED_TIME_SINCE ago
